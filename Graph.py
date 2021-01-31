8867e0afb1e0804a70c7fe25d0e8b3f3e3526019
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-01-31 20:11:43 +0330

    set_e_wight method for graph class added

--- Graph.py
+++ Graph.py
@@ -64,6 +64,28 @@ class Graph(object):
         return ((v.v_id(), u.v_id()) in self.__e.keys())
 
 
+    def set_e_wight(self, v: Vertice, u: Vertice, wight: float) -> None:
+
+        if self.has_e(v, u):
+            self.__e[(v.v_id(), u.v_id())] = wight
+            self.__e[(u.v_id(), v.v_id())] = wight
+
+            for neighbor in range(self.__adj[v.v_id()]):
+                if neighbor[0] == u.v_id():
+                    if neighbor[1] != wight:
+                        self.__adj[v.v_id()].remove(neighbor)
+                        self.__adj[v.v_id()].append((u.v_id(), wight))
+                    break
+
+            for neighbor in range(self.__adj[u.v_id()]):
+                if neighbor[0] == v.v_id():
+                    if neighbor[1] != wight:
+                        self.__adj[u.v_id()].remove(neighbor)
+                        self.__adj[u.v_id()].append((v.v_id(), wight))
+                    break
+
+
+
     def e_wight(self, v: Vertice, u: Vertice) -> float:
         if not self.has_e(v, u):
             return -1
